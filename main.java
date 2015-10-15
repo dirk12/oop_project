2090b2c17f48d62f0ce403919d8d5af54842cf1d
Tim Weigand <tim@weitiwo.de>
2015-10-15 10:09:15 +0000

    Fixed Example

--- main.java
+++ main.java
@@ -1,3 +1,9 @@
-public void main(){
-  System.out.println("Hello World");
+public class HelloWorld 
+{
+ 
+       public static void main (String[] args)
+       {
+             // Output Hello World!
+             System.out.println("Hello World!");
+       }
 }
